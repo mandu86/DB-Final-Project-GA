26c5a6baa11593c71b6aca9a76d5a9c4c99dc928
mandu86 <30415720+mandu86@users.noreply.github.com>
2017-08-21 19:08:02 +0100

    Initial Upload to Git

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('it works');
+
+$( document ).ready(function() {
+  // Handler for .ready() called.
+});
+
+$(function() {
+    var images = ['images/BG1.jpg', 'images/BG2.jpg'];
+    $('#background').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+   });
